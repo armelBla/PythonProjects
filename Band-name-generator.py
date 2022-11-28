2816d9b09d23b70a9b09353c960d3c5af94324cb
armelBla <110736523+armelBla@users.noreply.github.com>
2022-11-28 18:08:17 -0500

    Add project day 1

---
+++ Band-name-generator.py
@@ -0,0 +1,9 @@
+
+print('welcome to the band name generator.')
+
+city_life = input('Which city do you grow up in:\n')
+
+pet_name = input('What is  the pet name?:\n')
+
+band_name_generator = city_life + ' ' + pet_name
+print(f'your band name generator is {band_name_generator}.')
